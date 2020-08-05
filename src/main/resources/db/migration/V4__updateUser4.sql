9a8d86992927f82234450aba5571b544004ba65e
Divanio Silva <dcsilva@mail.ru>
2020-08-05 14:44:34 +0100

    Add two more users and update it

---
+++ src/main/resources/db/migration/V4__updateUser4.sql
@@ -0,0 +1,3 @@
+UPDATE project.REGISTRATION_USERS
+SET userName='lara_liberati', firstName='Lara', lastName='Liberati'
+WHERE id=4;
---
+++ target/classes/db/migration/V3__addNewUsers.sql
@@ -0,0 +1,2 @@
+INSERT INTO REGISTRATION_USERS(id, userName) VALUES (3, 'Marianna_Liberati');
+INSERT INTO REGISTRATION_USERS(id, userName) VALUES (4, 'Divanio_Silva');
